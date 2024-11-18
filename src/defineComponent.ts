22f36f19b14dea9403ad4f93096509dd3ad1c572
Aaron <aaronklinker1@gmail.com>
2024-11-17 20:39:40 -0600

    docs: Add missing JSDoc for defineComponent

--- src/defineComponent.ts
+++ src/defineComponent.ts
@@ -1,5 +1,23 @@
 import type { Component, ParentProps } from "npm:solid-js@1";
 
+/**
+ * Shortcut for defining a component with types.
+ *
+ * ```ts
+ * export default defineComponent<{ disabled?: boolean }>(props => {
+ *   props.disabled;
+ *   // ...
+ * })
+ * ```
+ *
+ * Optionally set the second type param to `true` to extend `ParentProps`.
+ *
+ * ```ts
+ * export default defineComponent<{ disabled?: boolean }, true>(props => {
+ *   return <div>{props.children}</div>
+ * })
+ * ```
+ */
 export function defineComponent<
   TProps extends Record<string, unknown> = EmptyProps,
   TParent extends boolean = false,
